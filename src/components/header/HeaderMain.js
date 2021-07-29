57af4dbc1f3c11c808aa84cc969c4f877282a982
zithachitra <69644637+ZithaChitra@users.noreply.github.com>
2021-07-29 13:41:32 +0200

    main header section

--- src/components/header/HeaderMain.js
+++ src/components/header/HeaderMain.js
@@ -1,27 +1,30 @@
 import React from 'react'
 import Navbar from './navbar/Navbar'
-import headerImg from "./images/headerimg2.jpeg"
+import backImage from "./images/headerimg2.jpeg"
+import "./HeaderMain.css"
 
 
+// light-blue darken-1
 function HeaderMain() {
     return (
         <React.Fragment>
             <Navbar />
-            <div id="index-banner" class="parallax-container">
-                <div class="section no-pad-bot">
+            <div
+                id="index-banner" className="parallax-container headermain">
+                <div className="section no-pad-bot">
                     <div class="container">
                         <br></br>
-                        <h3 class="header center teal-text text-lighten-2">David Livingstone Memorial High School</h3>
+                        <h3 class="header center white-text text-darken-1">David Livingstone Memorial High School</h3>
                         <div class="row center">
-                            <h5 class="header col s12 light">We can do it</h5>
+                            <h5 class="header col s12 light white-text">We can do it</h5>
                         </div>
                         <div class="row center">
-                            <a href="http://materializecss.com/getting-started.html" id="download-button" class="btn-large waves-effect waves-light teal lighten-1">Get Started</a>
+                            <a href="#" id="download-button" class="btn-large waves-effect waves-light light-blue darken-1">Learn more</a>
                         </div>
                         <br></br>
                     </div>
                 </div>
-                <div class="parallax"><img src={headerImg} alt="Unsplashed background img 1" /></div>
+                {/* <div className="parallax"><img src={backImage} alt="Unsplashed background img 1" /></div> */}
             </div>
         </React.Fragment>
     )
