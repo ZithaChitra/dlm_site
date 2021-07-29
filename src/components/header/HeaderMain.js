00e51475ea3e81f6fecc7fcd5a6e28a455cdc84a
zithachitra <69644637+ZithaChitra@users.noreply.github.com>
2021-07-29 18:01:02 +0200

    header section main

--- src/components/header/HeaderMain.js
+++ src/components/header/HeaderMain.js
@@ -9,22 +9,23 @@ function HeaderMain() {
     return (
         <React.Fragment>
             <Navbar />
-            <div
-                id="index-banner" className="parallax-container headermain">
-                <div className="section no-pad-bot">
-                    <div class="container">
-                        <br></br>
-                        <h3 class="header center white-text text-darken-1">David Livingstone Memorial High School</h3>
-                        <div class="row center">
-                            <h5 class="header col s12 light white-text">We can do it</h5>
+            <div id="index-banner" className="parallax-container headermain">
+                <div className="overlay">
+                    <div className="section no-pad-bot">
+                        <div class="container">
+                            <br></br>
+                            <h3 class="header center white-text text-darken-1">David Livingstone Memorial High School</h3>
+                            <div class="row center">
+                                <h5 class="header col s12 light white-text">We can do it</h5>
+                            </div>
+                            <div class="row center">
+                                <a href="#" id="download-button" class="btn-large waves-effect waves-light light-blue darken-1">Learn more</a>
+                            </div>
+                            <br></br>
                         </div>
-                        <div class="row center">
-                            <a href="#" id="download-button" class="btn-large waves-effect waves-light light-blue darken-1">Learn more</a>
-                        </div>
-                        <br></br>
                     </div>
+                    {/* <div className="parallax"><img src={backImage} alt="Unsplashed background img 1" /></div> */}
                 </div>
-                {/* <div className="parallax"><img src={backImage} alt="Unsplashed background img 1" /></div> */}
             </div>
         </React.Fragment>
     )
